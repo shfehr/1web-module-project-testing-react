2f0ace196d3c3f1b88839f53867648ab7b82b8f8
Steven Fehr <shfehr@gmail.com>
2024-01-19 16:22:26 -0600

    Show test is not allowing fireEvent

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -2,13 +2,53 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event';
 
-test('renders without errors', () => { });
 
-test('renders Loading component when prop show is null', () => { });
+jest.mock("./../../api/fetchShow");
 
-test('renders same number of options seasons are passed in', () => { });
+const testShow = {
+    name: "test show",
+    summary: "test summary",
+    seasons: [
+        {
+            id: 0,
+            name: "Season 1",
+            episode: []
+        },
+        {
+            id: 1,
+            name: "Season 2",
+            episode: []
+        }
+    ]
+}
 
-test('handleSelect is called when an season is selected', () => { });
+test('renders without errors', () => {
+    render(<Show show={testShow} selectedSeason = {"none"}/>)
+ });
+
+test('renders Loading component when prop show is null', () => { 
+    render(<Show show={null} />)
+    const loading = screen.queryByTestId("loading-container");
+    expect(loading).toBeInTheDocument()
+});
+
+test('renders same number of options seasons are passed in', () => {
+    render(<Show show={testShow} selectedSeason={"none"}/>);
+    const seasonOptions = screen.queryAllByTestId("season-option")
+    expect(seasonOptions).toHaveLength(2)
+ });
+
+test('handleSelect is called when an season is selected', () => { 
+    const handleSelect = jest.fn()
+    
+    render(<Show show={testShow} selectedSeason={"none"} handleSelect={handleSelect}/>)
+    const select = screen.getByLabelText(/Select A Season/i);
+    fireEvent.selectOptions(select, ['1'])
+    
+    expect(handleSelect).toBeCalled()
+});
+ 
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
